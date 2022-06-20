cdbb663d29d0b517200c7a26f6d457a1482a1e16
Hasan Çağlar <hsncglr07@gmail.com>
2022-06-20 22:51:54 +0300

    Back-end project
    .net core back-end project

---
+++ BackendFinalProject/Business/Constants/Messages.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Constants
+{
+    public static class Messages
+    {
+        public static string ProductAdded = "Ürün eklendi.";
+        public static string ProductNameInvalid = "Ürün ismi geçersiz. Ürün ismi en az 2 karakter olmalıdır.";
+        public static string MaintenanceTime = "Ürünler listenelemedi. Sistem bakımda.";
+        public static string ProductsListed = "Ürünler listelendi.";
+    }
+}
